33bfd4631868cadb014adad867898d054e3aa85f
Jenny Lian <jenny@fingerfoodstudios.com>
2016-01-17 14:03:03 -0800

    found out how bower works

--- server/index.js
+++ server/index.js
@@ -2,19 +2,25 @@ var express = require('express');
 var app = express();
 var path = require('path');
 //serve up our index.html
+var clientDir = path.dirname(__dirname) + '/client';
+var pathHomePage= path.format({
+    root : "/",
+    dir : clientDir,
+    base : "index.html",
+    ext : ".html",
+    name : "file"
+});
+
+
 app.get('/', function (req, res) {
-    var pathHome= path.format({
-        root : "/",
-        dir : path.dirname(__dirname) + '/client',
-        base : "index.html",
-        ext : ".html",
-        name : "file"
-    });
-    res.sendFile(pathHome);
+    console.log("clientdir", clientDir);
+    res.sendFile(pathHomePage);
 });
 
 //Some thing abot grabbing the folder we will see about that
-//app.use('/static', express.static(__dirname + '/client'));
+//app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+app.use('/bower_components',  express.static(clientDir + '/bower_components'));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
