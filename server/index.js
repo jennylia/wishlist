72f4ee0320ef99046ba18ff9e65d5d08e5b3df68
Jenny Lian <jenny@fingerfoodstudios.com>
2016-01-17 13:45:04 -0800

    added the simple serve, but feels like I didn't do the inclding javascript right, try bower

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
-
+var path = require('path');
+//serve up our index.html
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    var pathHome= path.format({
+        root : "/",
+        dir : path.dirname(__dirname) + '/client',
+        base : "index.html",
+        ext : ".html",
+        name : "file"
+    });
+    res.sendFile(pathHome);
 });
+
+//Some thing abot grabbing the folder we will see about that
+//app.use('/static', express.static(__dirname + '/client'));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
