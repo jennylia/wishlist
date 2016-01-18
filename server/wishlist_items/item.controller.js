bae5e1f96ede7c84bc122ed46e112aa8e976a6c1
Jenny Lian <jenny@fingerfoodstudios.com>
2016-01-17 16:03:05 -0800

    I think we are good

--- server/wishlist_items/item.controller.js
+++ server/wishlist_items/item.controller.js
@@ -2,7 +2,8 @@
  * Created by jlian on 2016-01-17.
  */
 
-var Item = require('./item.model')
+var Item = require('./item.model');
+var _ = require('lodash');
 module.exports.create = function(req,res){
     console.log(req.body);
     //console.log("hey hey");
@@ -13,14 +14,20 @@ module.exports.create = function(req,res){
             throw err;
 
         console.log("coming from mongoose", result);
-        res.json(result);
+        res.json(result); // same as sent I guess
     });
 
 }
 
 module.exports.list = function(req,res){
     console.log(req.body);
-    Item.find({}, function(err,res){
-        console.log(arguments);
+    Item.find({}, function(err,data){
+        /*
+        * Angular's front end will do the plucking for you
+        * They don't like it when you do this*/
+        //var nameList = _.map(data, 'name');
+        //console.log(nameList);
+        res.send(data);
+
     })
 }
